0d4f823f0a3308bddd06a72648d2e9841315d8ce
musty <mustyeneye@gmail.com>
2024-03-17 23:33:04 -1100

    adding projects todo and blogapi

---
+++ blogapi/shell/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\musty\OneDrive\Desktop\python_work\corr\shell\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
